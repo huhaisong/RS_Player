417ed8f736d470f7a4ab0b980276164acd81bb8b
eric <125468513@gmail.com>
2020-05-30 17:00:39 +0800

    修改空指针bug

--- 14RsWithTP/src/main/java/com/example/hu/mediaplayerapk/service/WorkTimerService.java
+++ 14RsWithTP/src/main/java/com/example/hu/mediaplayerapk/service/WorkTimerService.java
@@ -148,6 +148,10 @@ public class WorkTimerService extends Service {
                 }
                 if (beaconBean != null)
                     scheduleBeanArrayList = beaconBean.getScheduleBeans();
+                else {
+//                    MainActivity.isPlayingBeaconEvent = false;
+                    isSchedule = false;
+                }
                 return;
             }
             if (isEVENT) {
